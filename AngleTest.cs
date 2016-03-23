6eba9bff81f7912358a6106233d29aaab877e92a
HS <HS@HSui-MacBook-Air.local>
2016-03-23 12:27:56 +0900

    measure Angle

---
+++ AngleTest.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AngleTest : MonoBehaviour {
+
+	public Transform target;
+	public Text angleText;
+
+	//case) using input vector
+	//public Vector3 cu1;
+	//public Vector3 cu2;
+
+	//case) using object
+	public GameObject cube1;
+	public GameObject cube2;
+
+	public static float GetAngle(Vector3 vStart, Vector3 vEnd){
+		Vector3 v = vEnd - vStart;
+		return Mathf.Atan2 (v.y, v.x) * Mathf.Rad2Deg;
+	}
+		
+
+	// Update is called once per frame
+	void Update () {
+		//Vector3 relative = transform.InverseTransformPoint (target.position);
+		//float angle = Mathf.Atan2 (relative.x, relative.z) * Mathf.Rad2Deg;
+
+		angleText.text = "Angle: " + GetAngle (cube1.transform.InverseTransformPoint (target.position), cube2.transform.InverseTransformPoint (target.position)).ToString ();
+	}
+}
