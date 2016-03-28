45d0577c7de74a65a4d90db9194105cdd327751c
HS <HS@HSui-MacBook-Air.local>
2016-03-28 17:56:22 +0900

    resources

---
+++ test_Resource/SceneMove.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneMove : MonoBehaviour {
+
+	public void Click(){
+		SceneManager.LoadScene ("_Scene/MiniGame");	
+	}
+}
